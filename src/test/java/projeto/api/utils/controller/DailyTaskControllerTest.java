80188318ba8ba363cd0f03e67472ca819fb85568
joaopaulo <joaopaulosg17@gmail.com>
2024-08-04 18:33:32 -0300

    test: Adicionado test para metodo findAllByUser no controller de DailyTaskController

--- src/test/java/projeto/api/utils/controller/DailyTaskControllerTest.java
+++ src/test/java/projeto/api/utils/controller/DailyTaskControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,11 +19,13 @@ import projeto.api.utils.domain.user.UserDataDetails;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
@@ -34,6 +38,9 @@ class DailyTaskControllerTest {
 
     @Autowired
     private JacksonTester<DailyTaskDataDetailsDTO> dailyTaskDataDetailsJson;
+
+    @Autowired
+    private JacksonTester<Page<DailyTaskDataDetailsDTO>> pageJacksonTester;
 
     @Autowired
     private MockMvc mvc;
@@ -58,6 +65,27 @@ class DailyTaskControllerTest {
 
         var json = dailyTaskDataDetailsJson.write(details).getJson();
 
+        assertThat(response.getContentAsString()).isEqualTo(json);
+    }
+
+    @Test
+    @WithMockUser
+    void findAllByUserTest() throws Exception {
+        DailyTaskRegisterDataDTO data = new DailyTaskRegisterDataDTO("test name","description test",
+                LocalDateTime.now().plusHours(1), Priority.LOW);
+        UserDataDetails userDetails = new UserDataDetails(1L,"test","test@email.com");
+        DailyTaskDataDetailsDTO details = new DailyTaskDataDetailsDTO(1L,new DailyTask(data,new User()),userDetails);
+        List<DailyTaskDataDetailsDTO> dailyTasks = List.of(details);
+
+        Page<DailyTaskDataDetailsDTO> page = new PageImpl<>(dailyTasks);
+
+        when(service.findAllByUser(any(),any())).thenReturn(page);
+
+        var response = mvc.perform(get("/tasks")).andReturn().getResponse();
+
+        var json = pageJacksonTester.write(page).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(200);
         assertThat(response.getContentAsString()).isEqualTo(json);
     }
 }
